262b4575fd04189b7b1d909017b0376c1777e358
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-11-29 10:08:30 +0530

    [Amol]- Hello World

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter Name");
+String name=Console.ReadLine();
+Console.WriteLine("Enter lastname");
+String lastname=Console.ReadLine();
+Console.WriteLine("Hello {0} {1}", name, lastname);
